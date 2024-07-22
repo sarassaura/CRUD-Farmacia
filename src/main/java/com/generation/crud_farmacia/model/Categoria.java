c3a380f1d17b9bad3028c0e546b633c4276d9d4f
sarassaura <sarahyukinonakada@gmail.com>
2024-07-22 10:25:55 -0300

    feat: create product crud

--- src/main/java/com/generation/crud_farmacia/model/Categoria.java
+++ src/main/java/com/generation/crud_farmacia/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.crud_farmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
@@ -20,6 +27,10 @@ public class Categoria {
 	
 	@NotNull(message = "O Atributo Descrição é obrigatório")
 	private String descricao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
@@ -43,5 +54,13 @@ public class Categoria {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+	
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 }
