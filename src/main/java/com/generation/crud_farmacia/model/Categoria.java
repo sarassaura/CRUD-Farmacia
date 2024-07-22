59f1029495f97cc7905125e66e960cb34a885190
sarassaura <sarahyukinonakada@gmail.com>
2024-07-22 10:15:14 -0300

    feat: create crud categoria

---
+++ src/main/java/com/generation/crud_farmacia/model/Categoria.java
@@ -0,0 +1,47 @@
+package com.generation.crud_farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "O Atributo Nome é obrigatório")
+	private String nome;
+	
+	@NotNull(message = "O Atributo Descrição é obrigatório")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
