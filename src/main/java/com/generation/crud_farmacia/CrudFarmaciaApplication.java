dc1efc7d42746d48cbdc58b84bf187d1024f3762
sarassaura <sarahyukinonakada@gmail.com>
2024-07-22 09:55:46 -0300

    build: configure project

---
+++ src/main/java/com/generation/crud_farmacia/CrudFarmaciaApplication.java
@@ -0,0 +1,14 @@
+package com.generation.crud_farmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class CrudFarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudFarmaciaApplication.class, args);
+	}
+
+}
